984baaa70dbe4d9f6c11009978c3972d99933523
MOTONISHI MIDORI <midoo.nijo@gmail.com>
2020-03-06 14:43:56 +0000

    ログイン機能実装まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'home#top'
 
+  get '/signup', to: 'users#new'
+  
+  #ログイン
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+
  resources :users
 
 end
