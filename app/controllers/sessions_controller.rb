3e1833cfac0aef166d890e1df6c17bd3022d13e5
MOTONISHI MIDORI <midoo.nijo@gmail.com>
2020-03-07 14:54:00 +0000

    ログイン機能完成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,8 @@ class SessionsController < ApplicationController
   end
   
   def destroy
+    log_out
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_url
   end
-  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "アカウントを新規作成しました。"
       redirect_to @user
     else
@@ -18,7 +19,6 @@ class UsersController < ApplicationController
   end
   
   def edit
-    
   end
   
   private
