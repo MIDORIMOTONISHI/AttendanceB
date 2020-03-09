a62be62f263eaa77e4f82b15928f614c6078315e
MOTONISHI MIDORI <midoo.nijo@gmail.com>
2020-03-09 07:33:25 +0000

    フレンドリーフォワーディングまで

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,27 +1,33 @@
 module SessionsHelper
 
+  # ログイン
   def log_in(user)
     session[:user_id] = user.id
   end
   
+  # 永続的セッションを記憶
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  # 永続的セッションを破棄
   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
   end
   
+  # セッションと@current_userを破棄
   def log_out
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
   
+  # 一時的セッションにいるユーザーを返す
+  # それ以外の場合は、クッキーに対応するユーザーを返す
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -34,7 +40,24 @@ module SessionsHelper
     end
   end
   
+  # ユーザーがログイン済みのユーザーであれば、trueを返す
+  def current_user?(user)
+    user == current_user
+  end
+  
+  # 現在ログイン中のユーザーがいればtrue、そうでなければfalse
   def logged_in?
     !current_user.nil?
+  end
+  
+  # 記憶しているURLにリダイレクト（またはデフォルトURL）
+  def redirect_back_or(default_url)
+    redirect_to(session[:forwarding_url] || default_url)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを記憶
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
