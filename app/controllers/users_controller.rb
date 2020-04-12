2aa81894bd0dbb583bf2842e3a7a6a908535356e
MOTONISHI MIDORI <midoo.nijo@gmail.com>
2020-04-12 05:21:54 +0000

    追加機能№9まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,12 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_user, only: [:index, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_or_correct_user, only: :show
   before_action :set_one_month, only: :show
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page]).search(params[:search])
   end
 
   def show
@@ -50,12 +51,16 @@ class UsersController < ApplicationController
   end
   
   def update_basic_info
-    if @user.update_attributes(basic_info_params)
-      flash[:success] = "#{@user.name}の基本情報を更新しました。"
-    else
-      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    @users = User.all
+    @user.each do |user|
+      if users.update_attributes(basic_info_params)
+        flash[:success] = "勤怠情報を更新しました。"
+        redirect_to users_url and return
+      else
+        flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+        redirect_to edit_basic_info_user_url and return
+      end
     end
-    redirect_to users_url
   end
   
   private
@@ -65,6 +70,6 @@ class UsersController < ApplicationController
     end
     
     def basic_info_params
-      params.require(:user).permit(:department2, :basic_time, :work_time)
+      params.require(:user).permit(:basic_time, :work_time)
     end
 end
